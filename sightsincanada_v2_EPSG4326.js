048505b09dadb1261b0d1306c2d0b9def2249528
WortalMombat <WortalMombat@users.noreply.github.com>
2019-04-12 23:16:29 +0200

    js instead of geojson

---
+++ sightsincanada_v2_EPSG4326.js
@@ -0,0 +1,11 @@
+{
+"type": "FeatureCollection",
+"name": "sightsincanada_v2_EPSG4326",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 1, "name": "CN Tower", "info": "Toronto's top sight", "lat": null, "lon": null, "visited": null, "photo": null }, "geometry": { "type": "Point", "coordinates": [ -79.38714406411917, 43.642555711046583 ] } },
+{ "type": "Feature", "properties": { "id": 2, "name": "Toronto Airport", "info": "Arrival", "lat": null, "lon": null, "visited": null, "photo": null }, "geometry": { "type": "Point", "coordinates": [ -79.395968024928592, 43.6316269614615 ] } },
+{ "type": "Feature", "properties": { "id": 3, "name": "Royal Ontario Museum", "info": "An important museum", "lat": null, "lon": null, "visited": null, "photo": null }, "geometry": { "type": "Point", "coordinates": [ -79.394850251573956, 43.66793696265443 ] } },
+{ "type": "Feature", "properties": { "id": 4, "name": "Distillery District", "info": "Booze", "lat": null, "lon": null, "visited": null, "photo": null }, "geometry": { "type": "Point", "coordinates": [ -79.359919834240713, 43.650488504569232 ] } }
+]
+}
